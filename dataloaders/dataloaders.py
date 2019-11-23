b7eea645e6a56521e68d916b2a8360719d146b4f
alecokas <alecokastanos@gmail.com>
2019-11-23 23:15:52 +0000

    Train validation split

--- dataloaders/dataloaders.py
+++ dataloaders/dataloaders.py
@@ -5,16 +5,31 @@ from utils.utils import get_dataset
 
 
 def create_dataloaders(args):
-    """ """
+    """ Create traing and validation set generators """
     dataset = get_dataset(args.dataset_name)
 
-    # # Apparently this is slow - no idea why:
-    # # https://www.tensorflow.org/tutorials/load_data/images#load_using_tfdata
-    # image_generator = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
-    # train_generator = image_generator.flow_from_directory(
-    #     directory=dataset.path,
-    #     batch_size=args.batch_size,
-    #     shuffle=True,
-    #     target_size=(dataset.height, dataset.width),
-    #     classes=dataset.classes
-    # )
+    # No data augmentation for now
+    image_generator = tf.keras.preprocessing.image.ImageDataGenerator(
+        rescale=1./255,
+        # shear_range=0.2,
+        # zoom_range=0.2,
+        # horizontal_flip=True,
+        validation_split=0.1
+    )
+    train_generator = image_generator.flow_from_directory(
+        directory=dataset.directory,
+        batch_size=args.batch_size,
+        shuffle=True,
+        target_size=(dataset.height, dataset.width),
+        classes=dataset.classes,
+        subset='training'
+    )
+    val_generator = image_generator.flow_from_directory(
+        directory=dataset.directory,
+        batch_size=args.batch_size,
+        shuffle=True,
+        target_size=(dataset.height, dataset.width),
+        classes=dataset.classes,
+        subset='validation'
+    )
+    return train_generator, val_generator
