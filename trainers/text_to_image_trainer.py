bug fix: disjoint gradients affecting generator training

trainers/text_to_image_trainer.py
@@ -58,6 +58,7 @@ class TextToImageTrainer(Trainer):
                     )
                     img = np.asarray(img, dtype='float32')
                     wrong_img = np.asarray(wrong_img, dtype='float32')
+                    txt = np.asarray(txt, dtype='float32')
                     if self.augment:
                         img = transform_image(img)
                         wrong_img = transform_image(wrong_img)
@@ -73,7 +74,7 @@ class TextToImageTrainer(Trainer):
                         image_tensor.shape, wrong_image_tensor.shape
                     )
 
-                noise_z = tf.random.normal([batch_size, self.conditional_emb_size])
+                noise_z = np.random.normal(0, 1, (batch_size, 100)).astype('float32')
 
                 with tf.GradientTape() as generator_tape, tf.GradientTape() as discriminator_tape:
                     fake_images, mean, log_sigma = self.model.generator(text_tensor, noise_z, training=True)
@@ -83,9 +84,9 @@ class TextToImageTrainer(Trainer):
                             image_tensor.shape, fake_images.shape
                         )
 
-                    real_predictions = self.model.discriminator(image_tensor, text_tensor, training=True)
-                    wrong_predictions = self.model.discriminator(wrong_image_tensor, text_tensor, training=True)
-                    fake_predictions = self.model.discriminator(fake_images.numpy().astype('float32'), text_tensor, training=True)
+                    real_predictions = tf.squeeze(self.model.discriminator(image_tensor, text_tensor, training=True))
+                    wrong_predictions = tf.squeeze(self.model.discriminator(wrong_image_tensor, text_tensor, training=True))
+                    fake_predictions = tf.squeeze(self.model.discriminator(fake_images, text_tensor, training=True))
 
                     assert real_predictions.shape == wrong_predictions.shape == fake_predictions.shape, \
                         'Predictions for real ({}), wrong ({}) and fakes ({}) images must have the same dimensions'.format(