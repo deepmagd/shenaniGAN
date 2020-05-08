fa695c4ab0acaabc952670ae69be2ccac333ff62
alecokas <alecokastanos@gmail.com>
2020-05-08 18:16:20 +0100

    Major data refactor to include high and low res images in each TFRecord

--- dataloaders/dataloaders.py
+++ dataloaders/dataloaders.py
@@ -1,6 +1,4 @@
-import numpy as np
 import os
-import tensorflow as tf
 from utils.datasets import get_dataset
 from utils.utils import num_tfrecords_in_dir
 
@@ -8,14 +6,10 @@ from utils.utils import num_tfrecords_in_dir
 def create_dataloaders(args):
     """ Create traing and validation set generators """
     dataset = get_dataset(args.dataset_name)
-    if dataset.type == 'images':
-        return image_loaders(dataset, args.batch_size)
-    elif dataset.type == 'images-with-captions':
+    if dataset.type == 'images-with-captions':
         return image_with_captions_loaders(dataset, args.batch_size)
     elif dataset.type == 'images-with-tabular':
         return image_with_tabular(dataset, args.batch_size)
-    elif dataset.type == 'flowers':
-        return image_loaders(dataset, args.batch_size)
     else:
         raise Exception('Unexpected dataset type: {}'.format(dataset.type))
 
@@ -33,7 +27,7 @@ def image_with_tabular(dataset, batch_size):
         batch_size=batch_size,
         subset='valid'
     )
-    return train_loader, test_loader, dataset.get_dims()
+    return train_loader, test_loader,  dataset.get_small_dims(), dataset.get_large_dims()
 
 def image_with_captions_loaders(dataset, batch_size):
     """ Read, prepare, and present the TFRecord data """
@@ -47,34 +41,7 @@ def image_with_captions_loaders(dataset, batch_size):
         batch_size=batch_size,
         subset='test'
     )
-    return train_loader, test_loader, dataset.get_dims()
-
-def image_loaders(dataset, batch_size):
-    # No data augmentation for now
-    image_generator = tf.keras.preprocessing.image.ImageDataGenerator(
-        rescale=1./255,
-        # shear_range=0.2,
-        # zoom_range=0.2,
-        # horizontal_flip=True,
-        validation_split=0.1
-    )
-    train_generator = image_generator.flow_from_directory(
-        directory=dataset.directory,
-        batch_size=batch_size,
-        shuffle=True,
-        target_size=(dataset.height, dataset.width),
-        classes=dataset.classes,
-        subset='training'
-    )
-    val_generator = image_generator.flow_from_directory(
-        directory=dataset.directory,
-        batch_size=batch_size,
-        shuffle=True,
-        target_size=(dataset.height, dataset.width),
-        classes=dataset.classes,
-        subset='validation'
-    )
-    return train_generator, val_generator, dataset.get_dims()
+    return train_loader, test_loader, dataset.get_small_dims(), dataset.get_large_dims()
 
 
 class ImageTextDataLoader(object):
@@ -94,5 +61,4 @@ class ImageTextDataLoader(object):
             yield sample
 
     def __len__(self):
-        """ TODO: Check that the length is not incorrect because of the addition of batch sizes """
         return num_tfrecords_in_dir(os.path.join(self.dataset_object.directory, self.subset))
