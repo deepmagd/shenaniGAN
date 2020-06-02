996ae6715baeae409c63a6195a1bae4556936a4c
Devin Taylor <dev.t03@gmail.com>
2020-06-02 19:53:54 +0200

    test: ignore wrong loss

--- shenanigan/models/stackgan/stage1/model.py
+++ shenanigan/models/stackgan/stage1/model.py
@@ -222,5 +222,5 @@ class DiscriminatorStage1(Discriminator):
         real_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.fill(predictions_on_real.shape, 0.9), logits=predictions_on_real))
         wrong_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.zeros_like(predictions_on_wrong), logits=predictions_on_wrong))
         fake_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.zeros_like(predictions_on_fake), logits=predictions_on_fake))
-        total_loss = real_loss + (wrong_loss + fake_loss) / 2
+        total_loss = real_loss + fake_loss
         return total_loss, real_loss, wrong_loss, fake_loss
